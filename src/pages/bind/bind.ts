3d38bba0de4c97489a50009a5fa377138996c68a
thermafeel <thermafeel@naver.com>
2018-05-29 21:51:53 +0900

    2일차교육

---
+++ src/pages/bind/bind.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the BindPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-bind',
+  templateUrl: 'bind.html',
+})
+export class BindPage {
+  myValue:string;
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.myValue = navParams.get('value');
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad BindPage');
+  }
+
+  goBack() {
+    this.navCtrl.pop();
+  }
+
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,44 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+/**
+ * Generated class for the HomePage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+//@IonicPage({name:'hompage'})
+@IonicPage()
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
+  disabledSwitch = false;
+  userName:any;
 
-  constructor(public navCtrl: NavController) {
+  items = [
+    {"id" : 1, name:"첫번째 아이템"},
+    {"id" : 2, name:"두번째 아이템"},
+    {"id" : 3, name:"세번째 아이템"},
+    {"id" : 4, name:"네번째 아이템"}
+  ];
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
 
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad HomePage');
+  }
+
+  itemClicked(item) {
+    alert(item.name);
+  }
+
+  buttonClicked(event) {
+    // alert(event.target.textContent + " " + event.currentTarget.value);
+    console.log(event.clientX + ' : ' + event.clientY);
+    //bind page 호출
+    this.navCtrl.push( 'BindPage', {value:event.currentTarget.value});
   }
 
 }
